b2a25af04a8e2ab52303a819a0f9a1ce56511129
manhcuongqta <manhcuongqta@gmail.com>
2026-03-29 01:08:56 +0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js"></script>
+
+<script>
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+</script>
